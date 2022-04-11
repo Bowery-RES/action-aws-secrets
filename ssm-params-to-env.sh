33f8a6668aefaa16329a3d5b7847a6e2611c54ee
ArtemySinitsa <artsemi.sinitsa@itechart-group.com>
2022-04-11 20:19:56 +0300

    Add logs

--- ssm-params-to-env.sh
+++ ssm-params-to-env.sh
@@ -12,7 +12,7 @@ if [ -z "$parameters" ]; then
 fi
 
 format_var_name () {
-  echo "$1"
+  
   echo "$1" | awk -v prefix="$prefix" -F. '{print prefix $NF}' | tr "[:lower:]" "[:upper:]"
 }
 
@@ -23,12 +23,14 @@ get_ssm_param() {
     ssm_param_value=$(echo "$ssm_param" | jq '.Parameter.Value | fromjson')
     if [ -n "$simple_json" ] && [ "$simple_json" == "true" ]; then
       for p in $(echo "$ssm_param_value" | jq -r --arg v "$prefix" 'to_entries|map("\(.key)=\(.value|tostring)")|.[]' ); do
+        echo "$var_name"
         IFS='=' read -r var_name var_value <<< "$p"
         echo "$(format_var_name "$var_name")=$var_value" >> $GITHUB_ENV
       done
     else
       IFS=' ' read -r -a params <<< "$jq_filter"
       for var_name in "${params[@]}"; do
+        echo "$var_name"
         var_value=$(echo "$ssm_param_value" | jq -r -c "$var_name")
         echo "$(format_var_name "$var_name")=$var_value" >> $GITHUB_ENV
       done
