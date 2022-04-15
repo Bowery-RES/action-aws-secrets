bcfd241a70bbd9627517cc084e2b624133a82a11
ArtemySinitsa <artsemi.sinitsa@itechart-group.com>
2022-04-15 13:28:46 +0300

    Add logs

--- omit-secrets-namespace.sh
+++ omit-secrets-namespace.sh
@@ -8,6 +8,7 @@ for secret in $(echo $secrets | sed "s/,/ /g"); do
   full_name=$(echo "$secret" | tr / _ | awk -v prefix="$prefix" -F. '{print prefix $NF}' | tr "[:lower:]" "[:upper:]")
   name=$(echo "$secret" | sed 's/^.//' | awk -F/ '{print $NF}' | tr "[:lower:]" "[:upper:]")
   echo "$full_name"
+  echo "$secret" | awk 'BEGIN{FS=OFS="/"}{NF--; print}' | tr / _ | tr "[:lower:]" "[:upper:]"
   echo "$name"
   sed -i "s/$full_name/$name/" $GITHUB_ENV
 done
