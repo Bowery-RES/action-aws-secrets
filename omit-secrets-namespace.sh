ccea04d7e2a12528c5d38c6228b46846d89acf92
ArtemySinitsa <artsemi.sinitsa@itechart-group.com>
2022-04-15 14:51:50 +0300

    Remove prefix

--- omit-secrets-namespace.sh
+++ omit-secrets-namespace.sh
@@ -6,11 +6,7 @@ secrets="$SECRETS"
 
 for secret in $(echo $secrets | sed "s/,/ /g"); do
   full_name=$(echo "$secret" | tr / _ | awk -v prefix="$prefix" -F. '{print prefix $NF}' | tr "[:lower:]" "[:upper:]")
-  name=$(echo "$secret" | sed 's/^.//' | awk -F/ '{print $NF}' | tr "[:lower:]" "[:upper:]")
-  echo "$full_name"
-  echo "$secret" | awk 'BEGIN{FS=OFS="/"}{NF--; print}' | tr / _ | tr "[:lower:]" "[:upper:]"
-  echo "$name"
-  cat $GITHUB_ENV | sed -i "s/$full_name/$name/" > .test
-  cat .test
-  sed -i "s/$full_name/$name/" $GITHUB_ENV
+#   name=$(echo "$secret" | sed 's/^.//' | awk -F/ '{print $NF}' | tr "[:lower:]" "[:upper:]")
+  prefix=$(echo "$secret" | awk 'BEGIN{FS=OFS="/"}{NF--; print}' | tr / _ | tr "[:lower:]" "[:upper:]")
+  cat .env | sed -n -e "/^$full_name/p" | sed  "s/$full_name//" | cut -c2- >> $GITHUB_ENV
 done
