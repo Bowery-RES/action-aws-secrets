cd80a076f80310e48d4a3e46447f53dbfc196810
ArtemySinitsa <artsemi.sinitsa@itechart-group.com>
2022-04-15 14:57:24 +0300

    Change removal

--- omit-secrets-namespace.sh
+++ omit-secrets-namespace.sh
@@ -8,5 +8,5 @@ for secret in $(echo $secrets | sed "s/,/ /g"); do
   full_name=$(echo "$secret" | tr / _ | awk -v prefix="$prefix" -F. '{print prefix $NF}' | tr "[:lower:]" "[:upper:]")
 #   name=$(echo "$secret" | sed 's/^.//' | awk -F/ '{print $NF}' | tr "[:lower:]" "[:upper:]")
   prefix=$(echo "$secret" | awk 'BEGIN{FS=OFS="/"}{NF--; print}' | tr / _ | tr "[:lower:]" "[:upper:]")
-  printenv | sed -n -e "/^$full_name/p" | sed  "s/$full_name//" | cut -c2- >> $GITHUB_ENV
+  printenv | sed -n -e "/^$full_name/p" | sed  "s/$full_name//" | sed 's/^_//' >> $GITHUB_ENV
 done
