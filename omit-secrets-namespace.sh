2abd6f678f07e11567be28efdfe6dc0fbaf21902
ArtemySinitsa <artsemi.sinitsa@itechart-group.com>
2022-04-15 12:39:46 +0300

    Logs

--- omit-secrets-namespace.sh
+++ omit-secrets-namespace.sh
@@ -7,5 +7,7 @@ secrets="$SECRETS"
 for secret in $(echo $secrets | sed "s/,/ /g"); do
   full_name=$(echo "$secret" | tr / _ | awk -v prefix="$prefix" -F. '{print prefix $NF}' | tr "[:lower:]" "[:upper:]")
   name=$(echo "$secret" | sed 's/^.//' | awk -F/ '{print $NF}' | tr "[:lower:]" "[:upper:]")
+  echo "$full_name"
+  echo "$name"
   sed -i "s/$full_name/$name/" $GITHUB_ENV
 done
