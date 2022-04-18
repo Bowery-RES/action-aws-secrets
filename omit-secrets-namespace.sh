3427da7b438b5a22f9f5915fad87b595528cb6b7
ArtemySinitsa <artsemi.sinitsa@itechart-group.com>
2022-04-18 12:01:26 +0300

    Fix secrets loop

--- omit-secrets-namespace.sh
+++ omit-secrets-namespace.sh
@@ -4,8 +4,7 @@ set -e
 
 secrets="$SECRETS"
 
-for secret in $(echo $secrets | sed "s/,/ /g"); do
-  echo "$secret"
-  # full_name=$(echo "$secret" | tr / _ | awk -v prefix="$prefix" -F. '{print prefix $NF}' | tr "[:lower:]" "[:upper:]")
-  # printenv | sed -n -e "/^$full_name/p" | sed  "s/$full_name//" | sed 's/^_//' >> $GITHUB_ENV
+for secret in $(echo $secrets); do
+  full_name=$(echo "$secret" | tr / _ | awk -v prefix="$prefix" -F. '{print prefix $NF}' | tr "[:lower:]" "[:upper:]")
+  printenv | sed -n -e "/^$full_name/p" | sed  "s/$full_name//" | sed 's/^_//' >> $GITHUB_ENV
 done
