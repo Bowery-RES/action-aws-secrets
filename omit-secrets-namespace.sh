b7115aceb7d6d41774c5431f7f481a84bc14c574
ArtemySinitsa <artsemi.sinitsa@itechart-group.com>
2022-04-18 12:16:43 +0300

    Add formatting

--- omit-secrets-namespace.sh
+++ omit-secrets-namespace.sh
@@ -6,7 +6,7 @@ secrets="$SECRETS"
 
 for secret in $(echo $secrets | sed "s/%0A/\n/g"); do
   echo "$secret"
-  full_name=$(echo "$secret" | tr / _ | awk -v prefix="$prefix" -F. '{print prefix $NF}' | tr "[:lower:]" "[:upper:]")
+  full_name=$(echo "$secret" | tr / _ | tr - _ | awk -v prefix="$prefix" -F. '{print prefix $NF}' | tr "[:lower:]" "[:upper:]")
   echo "$full_name"
   printenv | sed -n -e "/^$full_name/p" | sed  "s/$full_name//" | sed 's/^_//' >> $GITHUB_ENV
 done
