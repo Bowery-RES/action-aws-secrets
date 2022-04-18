792d93fc34d8dc7670791907f410128acd5bf5d5
ArtemySinitsa <artsemi.sinitsa@itechart-group.com>
2022-04-18 11:34:17 +0300

    Check secrets

--- omit-secrets-namespace.sh
+++ omit-secrets-namespace.sh
@@ -5,6 +5,7 @@ set -e
 secrets="$SECRETS"
 
 for secret in $(echo $secrets | sed "s/,/ /g"); do
-  full_name=$(echo "$secret" | tr / _ | awk -v prefix="$prefix" -F. '{print prefix $NF}' | tr "[:lower:]" "[:upper:]")
-  printenv | sed -n -e "/^$full_name/p" | sed  "s/$full_name//" | sed 's/^_//' >> $GITHUB_ENV
+  echo "$secret"
+  # full_name=$(echo "$secret" | tr / _ | awk -v prefix="$prefix" -F. '{print prefix $NF}' | tr "[:lower:]" "[:upper:]")
+  # printenv | sed -n -e "/^$full_name/p" | sed  "s/$full_name//" | sed 's/^_//' >> $GITHUB_ENV
 done
